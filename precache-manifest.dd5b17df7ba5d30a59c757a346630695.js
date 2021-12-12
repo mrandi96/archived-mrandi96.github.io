6d527e682d2f858fcdf48a936e81f94ced0772f8
Muhammad Randi Pratama <randi@code.id>
2021-12-12 18:23:12 +0700

    Updates

---
+++ precache-manifest.dd5b17df7ba5d30a59c757a346630695.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60f11b56836750af270ee07204c73998",
+    "url": "/index.html"
+  },
+  {
+    "revision": "31047d145b6afc89f741",
+    "url": "/static/css/main.6dade7dd.chunk.css"
+  },
+  {
+    "revision": "3afc922b91de4636519e",
+    "url": "/static/js/2.4d8a2ae7.chunk.js"
+  },
+  {
+    "revision": "808d6f94adc40c7088c66d06c3f08799",
+    "url": "/static/js/2.4d8a2ae7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a6c7a56f507e0d6d1a16",
+    "url": "/static/js/3.a139e299.chunk.js"
+  },
+  {
+    "revision": "31047d145b6afc89f741",
+    "url": "/static/js/main.dede3d3a.chunk.js"
+  },
+  {
+    "revision": "2947ddb84996737103f9",
+    "url": "/static/js/runtime-main.e2bc92bd.js"
+  }
+]);
